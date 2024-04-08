bfa46a6a426b3c0af133955ef6dd81e400f99e1f
Matheus Souza <mmedeiros.cbp@gmail.com>
2024-04-08 20:05:26 -0300

    deleted PointDetailLeaderResponse.kt

--- app/src/main/java/br/com/atitude/finder/data/network/entity/response/pointdetail/PointDetailLeaderResponse.kt
+++
@@ -1,7 +0,0 @@
-package br.com.atitude.finder.data.network.entity.response.pointdetail
-
-import com.google.gson.annotations.SerializedName
-
-data class PointDetailLeaderResponse(
-    @SerializedName("leader_name") val leaderName: String
-)
