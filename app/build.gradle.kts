3fd6c4dee2cf261d51b0e2747cc6588fee8fbb65
Matheus Souza <mmedeiros.cbp@gmail.com>
2024-05-15 21:44:59 -0300

    remove map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "br.com.atitude.finder"
         minSdk = 24
         targetSdk = 34
-        versionCode = 20
-        versionName = "1.6.0"
+        versionCode = 21
+        versionName = "1.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
